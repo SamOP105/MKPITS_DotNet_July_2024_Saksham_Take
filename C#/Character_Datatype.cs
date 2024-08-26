0469a28a35dd8554a2234b951676fe82d15f1307
Saksham Take <sakshamtake69@gmail.com>
2024-08-26 11:30:26 +0530

    Character Datatype

---
+++ C#/Character_Datatype.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] character = { 'A', 'B', 'C', 'D','E' ,'F' };
+            Console.WriteLine(character[1]);
+            Console.Read();
+        }
+    }
+}
