4b3c95d7072368b26d455b74f0e06efdc2ba4bd5
Saksham Take <sakshamtake69@gmail.com>
2024-08-28 14:38:32 +0530

    1-10_table

---
+++ C#/1-10_table.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Assignment_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <=10; j++)
+                {
+                    Console.WriteLine(i + " * " + j + " = " + i * j);
+                }
+            }Console.Read();
+        }
+    }
+}
+
