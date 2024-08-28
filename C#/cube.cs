fa69bfd6a5b4897030ebe531d04879683407fef1
Saksham Take <sakshamtake69@gmail.com>
2024-08-28 14:34:12 +0530

    Cube Of Number

---
+++ C#/cube.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Assignment_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Cube of "+i+" : " +i*i*i);
+            }Console.ReadLine();
+        }
+    }
+}
