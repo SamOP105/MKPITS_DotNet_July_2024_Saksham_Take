249ed0e33f780404387edad414b6886ca2df6ade
Saksham Take <sakshamtake69@gmail.com>
2024-08-28 13:15:43 +0530

    Check Student eligibility using if-else

---
+++ C#/Admission_check.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Ass_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Maths=Convert.ToInt32(Console.ReadLine());
+            int Chem=Convert.ToInt32(Console.ReadLine());
+            int Phy=Convert.ToInt32(Console.ReadLine());
+            int Total= Maths+Chem+Phy;
+            int MP=Maths+Phy;
+            if( (Maths >= 65 && Chem>=50 && Phy >= 55) && (Total>=190) && (MP>=140))
+            {
+                Console.WriteLine("The candidate is eligible for admission");
+            }else
+            {
+                Console.WriteLine("The candidate is not eligible for admission");
+            }
+            Console.ReadLine();
+        }
+    }
+}
