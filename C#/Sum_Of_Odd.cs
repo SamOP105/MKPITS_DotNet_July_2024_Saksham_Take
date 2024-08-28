b9297d0a0e5f1246c9300766a182535fba36843a
Saksham Take <sakshamtake69@gmail.com>
2024-08-28 14:40:27 +0530

    Sum Of Odd Numbers

---
+++ C#/Sum_Of_Odd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Assignment_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int n=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Odd numbers are: ");
+            for (int i = 1; i <=2*n; i++)
+            {
+                Console.WriteLine(i);
+                sum = sum + i;
+                i++;
+            }Console.WriteLine(sum);
+            Console.Read();
+        }
+    }
+}
