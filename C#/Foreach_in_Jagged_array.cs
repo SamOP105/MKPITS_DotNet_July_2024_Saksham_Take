512cfbe4f79f1056ff775d3d82c169dfc398072b
Saksham Take <sakshamtake69@gmail.com>
2024-08-29 10:57:22 +0530

    Foreach_Implementation_in_jagged_array

---
+++ C#/Foreach_in_Jagged_array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jagged_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] ladder = new int[4][]; 
+
+                ladder[0] = new int[]{ 1};
+            ladder[1] = new int[] { 2, 3 };
+            ladder[2] = new int[] { 4, 5, 6 };
+            ladder[3] = new int[] {7, 8, 9, 10 };
+
+            foreach (int[] x in ladder)
+            {
+                foreach (int var in x)
+                {
+                    Console.Write(var);
+                }Console.WriteLine();
+            }
+            Console.Read();
+        
+        } 
+    }
+}
