c1ef4563aa9b4264bd1f9c7f185b0380e8897cd4
Saksham Take <sakshamtake69@gmail.com>
2024-08-29 16:01:03 +0530

    Male Female Using Foreach

---
+++ C#/Male_female_foreach.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Male_Female_For_each
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countm=0; int countf = 0;
+            Console.WriteLine("Enter:");
+            int n=Convert.ToInt32(Console.ReadLine());
+            char[]  c= new char[n];
+            
+            for (int row=0; row<n;row++)
+            {
+                c[row] = Convert.ToChar(Console.ReadLine());
+
+            }
+            foreach (char row in c)
+            {
+                if (row == 'm' || row == 'M')
+                {
+                    countm++;
+                }
+                else if (row == 'F' || row == 'f')
+                {
+                    countf++;
+                }
+
+            }
+            Console.WriteLine(countm +"\t"+ countf);
+            Console.ReadLine();
+
+        }
+        
+    }
+}
