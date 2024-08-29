8aecbecc68b217b6f1ff9f83941260de0e39ee58
Saksham Take <sakshamtake69@gmail.com>
2024-08-29 10:39:42 +0530

    Implementation_Of_Dictonary

---
+++ C#/Dictonary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionry_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> myDict = new Dictionary<int,string>();
+            myDict.Add(1, "Saksham");
+            myDict.Add(2, "Neeraj");
+            myDict.Add(3, "Shruti");
+            myDict.Add(4, "Saqlin");
+            myDict.Add(5, "Raksha");
+            myDict.Add(6, "Nilesh");
+            foreach(KeyValuePair<int,string> elements in myDict)
+            {
+                Console.WriteLine(elements.Key+" "+elements.Value);
+            }Console.Read();
+
+        }
+    }
+}
