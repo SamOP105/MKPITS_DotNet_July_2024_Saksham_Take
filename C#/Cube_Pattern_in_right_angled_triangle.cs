66c933f36e952494afd4d577f4d01dce437128be
Saksham Take <sakshamtake69@gmail.com>
2024-08-29 10:32:54 +0530

    Cube Pattern in Right angle Triangle

---
+++ C#/Cube_Pattern_in_right_angled_triangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cube
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = 1; counter <= 5; counter++)
+            {
+                int x = 1;int y = 1;
+                for (int i = 1; i <= counter; i++)
+                {
+                    y=x* x;
+                    Console.Write(y);
+                    x++;
+                }Console.WriteLine();
+            }
+            Console.Read();
+        }   
+    }
+}
