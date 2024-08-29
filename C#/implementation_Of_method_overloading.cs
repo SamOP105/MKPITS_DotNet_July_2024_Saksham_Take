12d5b77ff579f749e302a30dd20074ce4fbf9554
Saksham Take <sakshamtake69@gmail.com>
2024-08-29 10:46:57 +0530

    Implementation_Of_Method_Overloading

---
+++ C#/implementation_Of_method_overloading.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function_1
+{
+    public class Accountholder
+    {
+        string Name;
+        int age;
+        public void getDetails()
+        {
+            Name = "Saksham";
+            age = 22;
+            Console.WriteLine("Account Holder Name: " + Name + "\nAge: " + age);
+        }
+        public void getDetails(int X)
+        {
+            Console.WriteLine(X);
+        }
+        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Accountholder ac= new Accountholder();
+            ac.getDetails();
+            ac.getDetails(43);
+            Console.Read();
+        }
+    }
+}
