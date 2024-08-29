2199309c8f94838ec6d7d0781c85c2e92b1415fe
Saksham Take <sakshamtake69@gmail.com>
2024-08-29 10:48:40 +0530

    Implementation_Of_Interface

---
+++ C#/Implementation_Of_Interface.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfaces_1
+{
+    class Base
+    {
+
+    }
+    interface infB
+    {
+        void infBase();
+    }
+    class Derived : Base, infB
+    {
+        public void infBase()
+        {
+            Console.WriteLine("L");
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Derived d = new Derived();
+            d.infBase();
+            Console.Read();
+        }
+    }
+}
