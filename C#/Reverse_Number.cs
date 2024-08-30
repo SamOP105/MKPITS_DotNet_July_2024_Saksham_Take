ec138fb46679b5f5d99d499d92a533b737727f5f
Saksham Take <sakshamtake69@gmail.com>
2024-08-30 16:46:49 +0530

    Reverse A Number

---
+++ C#/Reverse_Number.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=123,Reverse= 0;
+            while (n != 0)
+            {
+                int Remainder = n % 10;
+                n = n / 10;
+                Reverse = Reverse * 10 + Remainder;
+
+            }
+            Console.WriteLine(Reverse);
+            Console.Read();
+        }
+    }
+}
