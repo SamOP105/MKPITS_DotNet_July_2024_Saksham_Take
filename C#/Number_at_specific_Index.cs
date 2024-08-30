15ba41aa8e81961af4a05f854ff9403a6007d9b7
Saksham Take <sakshamtake69@gmail.com>
2024-08-30 16:53:51 +0530

    Find Index Number

---
+++ C#/Number_at_specific_Index.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search_given_element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count= Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                array[i]=Convert.ToInt32(Console.ReadLine());
+
+            }
+            int X=Convert.ToInt32(Console.ReadLine());
+            for (int i = 0;i < count; i++)
+            {
+                if (X == array[i])
+                {
+                    Console.WriteLine("Array index: "+i);
+                }
+            }Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
