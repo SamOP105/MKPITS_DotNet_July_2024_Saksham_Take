681592c925ff5b1d097ab558a35fe90344c15e39
Saksham Take <sakshamtake69@gmail.com>
2024-08-30 16:51:32 +0530

    S pattern

---
+++ C#/S_Pattern.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 1; row <= 7; row++)
+            {
+                for (int  col= 1; col <= 4; col++)
+                {
+                    if (row == 1 &&( col==2 || col==3 )||
+                        row==2 && (col==1 ||col==4 )||
+                        row == 3 && (col == 1 ) ||
+                        row == 4 &&(col == 2 || col == 3) || 
+                        row == 5 &&( col == 4) ||
+                        row == 6 && (col == 1 || col == 4) ||
+                        row == 7 && (col == 2 || col == 3)) {
+                        Console.Write("*");
+
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
