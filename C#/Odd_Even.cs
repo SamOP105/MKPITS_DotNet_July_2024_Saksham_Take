1b5eefd730fe2789ecc373bc9f123a568251fa66
Saksham Take <sakshamtake69@gmail.com>
2024-08-30 17:04:26 +0530

    Even_Odd_Number_Check

---
+++ C#/Odd_Even.cs
@@ -0,0 +1,29 @@
+﻿/*Write a C program to check whether a given number is even or odd.
+Test Data : 15
+Expected Output :
+15 is an odd integer*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n%2==0) {
+                Console.WriteLine(n+" is an even integer");
+            }
+            else
+            {
+                Console.WriteLine(n+" is an odd integer");
+            }
+           Console.ReadLine();
+
+        }
+    }
+}
