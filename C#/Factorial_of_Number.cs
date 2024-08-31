0ff8ecd421386991ec113b13edb6c332c7581d39
Saksham Take <sakshamtake69@gmail.com>
2024-08-31 19:22:02 +0530

    Factorial Of a Number

---
+++ C#/Factorial_of_Number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number: ");
+            int number=Convert.ToInt32(Console.ReadLine());
+            int Factorial = 1;
+            for (int itrate = 1; itrate <=number; itrate++)
+            {
+                Factorial=Factorial*itrate;
+            }
+            Console.WriteLine(Factorial);
+            Console.Read();
+        }
+    }
+}
