603134961c6d5a500089876286ab48f7795421a6
Saksham Take <sakshamtake69@gmail.com>
2024-09-04 23:59:53 +0530

    Simple Intrest

---
+++ C#/Simple_intreset.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9
+{
+    public class Interest
+    {
+        double principal;
+        double year;
+        double rate;
+
+        
+        public Interest()
+        {
+            Console.WriteLine("Enter: ");
+            principal= Convert.ToDouble(Console.ReadLine());
+            year =Convert.ToDouble(Console.ReadLine());
+            rate=Convert.ToDouble(Console.ReadLine());
+            
+        }
+
+        
+        public void InterestX()
+        {
+            double SimpleInterest= (principal*rate*year)/100;
+            Console.WriteLine("Simple Intreset:"+SimpleInterest);
+        }
+
+        
+        
+
+        static void Main(string[] args)
+        {
+            Interest In =new Interest();
+            In.InterestX();
+            Console.Read();
+
+           
+        }
+    }
+
+}
