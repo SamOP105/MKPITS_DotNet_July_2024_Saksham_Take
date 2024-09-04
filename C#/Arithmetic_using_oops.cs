b5a5c7dd45baf36c8af958c14c56bf6d5a70c808
Saksham Take <sakshamtake69@gmail.com>
2024-09-04 23:23:29 +0530

    Arithmetic Operation using OOPs concept

---
+++ C#/Arithmetic_using_oops.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8
+{
+    internal class Program
+    {
+        public class Operator
+        {
+            double num1, num2;
+            public Operator()
+            {
+                Console.WriteLine("Enter Two Numbers");
+                num1=Convert.ToDouble(Console.ReadLine());
+                num2=Convert.ToDouble(Console.ReadLine());
+            }
+            public void Add()
+            {
+                double Addition=num1 + num2;
+                Console.WriteLine(Addition);
+            }
+            public void Sub()
+            {
+                double Subition=num1 - num2;
+                    Console.WriteLine(Subition);
+
+            }
+            public void Mul()
+            {
+                double Mulition = num1 * num2;
+                Console.WriteLine(Mulition);
+            }
+            public void Div()
+            {
+                double Divition=num1 / num2;
+                Console.WriteLine(Divition);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Operator or = new Operator();
+            or.Add();
+            or.Sub();
+            or.Mul();
+            or.Div();
+            Console.Read();
+        }
+    }
+}
