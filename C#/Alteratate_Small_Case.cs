d1efea9f4904196781d56587300aba3575db49f3
Saksham Take <sakshamtake69@gmail.com>
2024-09-10 23:45:21 +0530

    Alternate Character Should be Small case

---
+++ C#/Alteratate_Small_Case.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace _12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "APPLE";
+            for (int iterate = 0; iterate < s.Length; iterate++)
+            {
+                // Check if the index is even
+                if (iterate % 2 == 0)
+                {
+                    // Print the character in uppercase
+                    Console.Write(char.ToUpper(s[iterate]));
+                }
+                else
+                {
+                    // Print the character in lowercase
+                    Console.Write(char.ToLower(s[iterate]));
+                }
+            }
+            Console.WriteLine();
+            Console.Read();
+        }
+    }
+}
+
