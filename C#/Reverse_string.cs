4f9f97101be14596c3e7ad2bc7f2e33399b07c2d
Saksham Take <sakshamtake69@gmail.com>
2024-10-03 22:57:38 +0530

    Reverse a string

---
+++ C#/Reverse_string.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            
+                Console.WriteLine("Enter a string:");
+                string S = Console.ReadLine();
+
+                char[] reversedArray = new char[S.Length];
+
+                for (int i = S.Length-1; i >=0 ; i--)
+                {
+                    Console.WriteLine(S[i]);
+                }
+
+                
+            Console.Read();
+            
+        }
+
+    }
+}
+
