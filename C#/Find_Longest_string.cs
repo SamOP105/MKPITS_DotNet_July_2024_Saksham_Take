fcd00ed315b4e0282430596348d2f2b64f87e767
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:54:39 +0530

    Find longest string

---
+++ C#/Find_Longest_string.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _39_Find_Longest_Word_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string input = Console.ReadLine();
+            string longest = "";
+            string current = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != ' ')
+                {
+                    current += input[i];
+                }
+                else
+                {
+                    if (current.Length > longest.Length)
+                    {
+                        longest = current;
+                    }
+                    current= "";
+                }
+            }
+            if (current.Length > longest.Length)
+            {
+                longest = current;
+            }
+            Console.WriteLine(longest);
+            Console.ReadLine();
+
+        }
+    }
+}
