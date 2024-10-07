e0c59f1f266333805e548a74a4435929a18ec0c3
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:23:11 +0530

    Digits in string

---
+++ C#/digits_in_string.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_digits_in_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
+
+            int d = 0;
+
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] >= '0' && s[i] <= '9')
+                {
+                    d++;
+                }
+            }
+            Console.WriteLine("Digits: " + d);
+            Console.Read();
+        }
+    }
+}
