a312449a54ac71da8ed8f28a3337420e4f72d50d
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:53:49 +0530

    Reverse word in a string

---
+++ C#/Reverse word ins tring.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38_Reverse_the_Words_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string input = Console.ReadLine();
+            string result = "";
+            string word = "";
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                if (input[i] != ' ')
+                {
+                    word = input[i] + word;
+                }
+                else
+                {
+                    result += word + ' ';
+                    word = "";
+                }
+            }
+            result += word; 
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+
+        }
+    }
+}
