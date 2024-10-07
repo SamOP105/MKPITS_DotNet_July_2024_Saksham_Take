b9041ce63fd2491a679f411b4dc4b6997ee3072e
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:43:53 +0530

    Capitlize the first letter of each word

---
+++ C#/Capitalize the first letter of each_word.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_Capitalize_the_First_Letter_of_Each_Word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string result = "";
+            bool capitalizeNext = true;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == ' ')
+                {
+                    result += ' ';
+                    capitalizeNext = true;
+                }
+                else
+                {
+                    if (capitalizeNext)
+                    {
+                        result += char.ToUpper(input[i]);
+                        capitalizeNext = false;
+                    }
+                    else
+                    {
+                        result += input[i];
+                    }
+                }
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
