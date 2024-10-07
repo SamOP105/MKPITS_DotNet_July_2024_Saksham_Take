79dd9ff80478ae7e64cfd2383851c41f4ecef35d
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:39:09 +0530

    Check if the character is in string

---
+++ C#/Check_for_character_in_string.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_Check_if_the_character_is_in_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char charToCheck = 'e';
+            bool found = false;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == charToCheck)
+                {
+                    found = true;
+                    break;
+                }
+            }
+            Console.WriteLine(found ? "Character found" : "Character not found");
+            Console.ReadLine();
+
+        }
+    }
+}
