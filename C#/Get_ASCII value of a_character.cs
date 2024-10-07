df0bb7873ecabe7bef8b59f4b630dc5dde173029
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:47:04 +0530

    Get the ASCII value of a Character

---
+++ C#/Get_ASCII value of a_character.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_Get_the_ASCII_Value_of_a_Character
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char c = 'A';
+            int asciiValue = c; 
+            Console.WriteLine(asciiValue);
+            Console.ReadLine();
+
+        }
+    }
+}
