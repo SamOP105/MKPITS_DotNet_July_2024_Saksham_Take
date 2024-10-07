51a6c0c1280e4046b474eaf5a919bdcf6b85efdd
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:16:07 +0530

    Reverse a string without using function

---
+++ C#/reverse_a_string.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string s = Console.ReadLine();
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                Console.Write(s[i]);
+            }
+            Console.Read();
+
+            
+        }
+    }
+}
