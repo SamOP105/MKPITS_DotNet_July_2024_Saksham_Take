6944c6d1de3823023c98a1d5a0a57515cea6ce43
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:19:03 +0530

    Count Word in string

---
+++ C#/Count_word.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_count_word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string s = Console.ReadLine();
+            int wordCount = 0;
+
+            
+            if (s.Length > 0 && s[0] != ' ')
+            {
+                wordCount++;
+            }
+
+        
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] != ' ' && s[i - 1] == ' ')
+                {
+                    wordCount++;
+                }
+            }
+
+            Console.WriteLine("Number of words: " + wordCount);
+            Console.Read();
+        }
+    }
+}
