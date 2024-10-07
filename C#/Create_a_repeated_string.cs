774c27956995175170baf77851d5d96aec9da0dc
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:48:01 +0530

    Create a repeated string

---
+++ C#/Create_a_repeated_string.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32_create_a_repeated_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string str =Console.ReadLine();
+            int times = 3;
+            string result = "";
+
+            for (int i = 0; i < times; i++)
+            {
+                result += str;
+            }
+            Console.WriteLine(result);
+
+            Console.Read();
+        }
+    }
+}
