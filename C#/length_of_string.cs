77df8612d79d995c0794d2fca4ab04c9b1a7e8d7
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:17:21 +0530

    Length of string

---
+++ C#/length_of_string.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_length_of_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string input = Console.ReadLine();
+            int length = 0;
+
+            foreach (char c in input)
+            {
+                length++;
+            }
+
+            Console.WriteLine("Length of the string: " + length);
+            Console.Read();
+        }
+    }
+}
