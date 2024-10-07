9b579e5a52b0c102a7efef738ffb9e6e18575ac5
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:45:08 +0530

    Count Number of sentences in string

---
+++ C#/Count_Number_of_sentences_in_string.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29_Count_the_Number_of_Sentences_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string input = Console.ReadLine();
+            int sentenceCount = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '.' || input[i] == '!' || input[i] == '?')
+                {
+                    sentenceCount++;
+                }
+            }
+            Console.WriteLine(sentenceCount);
+            Console.ReadLine();
+
+        }
+    }
+}
