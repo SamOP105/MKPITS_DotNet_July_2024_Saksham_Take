cdc0ac2a1761df85a10a1ca8eeeb1b22694ed042
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:20:00 +0530

    Occurence of character in string

---
+++ C#/Occurenece_of_string.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_occrence_of_character
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Enter the character to count:");
+            char charToCount = Console.ReadLine()[0];
+            int count = 0;
+
+            foreach (char c in input)
+            {
+                if (c == charToCount)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Character '" + charToCount + "' appears " + count + " times.");
+        }
+    }
+}
