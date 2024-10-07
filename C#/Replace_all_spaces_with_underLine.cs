fa0d5bcfe08f92fb4bb74276addd206f3edbbe52
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:40:40 +0530

    Replace all spaces with UnderLine

---
+++ C#/Replace_all_spaces_with_underLine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_replace_all_spaces_with_underline
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string result = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == ' ')
+                {
+                    result += '_';
+                }
+                else
+                {
+                    result += input[i];
+                }
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
