e51e734108c0322ccb6ff35aec5b03829c7132cd
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:29:56 +0530

    Last_Character

---
+++ C#/last_Character.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace last_character
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char lastChar = input[input.Length - 1];
+            Console.WriteLine(lastChar);
+            Console.ReadLine();
+
+        }
+    }
+}
