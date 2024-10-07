70537a3851f76a073bff9b61dd0ec74b8514e7c4
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:24:53 +0530

    Length

---
+++ C#/length.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_legth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String");
+            string input = Console.ReadLine();
+            
+            int length = 0;
+            foreach (char c in input)
+            {
+                length++;
+            }
+            Console.WriteLine(length);
+            Console.ReadLine(); 
+        }
+    }
+}
