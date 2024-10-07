13c55af9c7b9daf4963276981d1130624281cc71
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:28:41 +0530

    Upper Case

---
+++ C#/Upper_case.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Upper_case
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hello World";
+            string upperCase = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                if (c >= 'a' && c <= 'z')
+                {
+                    upperCase += (char)(c - 32);
+                }
+                else
+                {
+                    upperCase += c;
+                }
+            }
+            Console.WriteLine(upperCase);
+            Console.ReadLine();
+        }
+    }
+}
