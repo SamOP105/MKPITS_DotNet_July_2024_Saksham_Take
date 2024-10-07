e3efb767ae08acbeac03c39fb6489160b9d8e1a3
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:36:28 +0530

    Check whether a string is empty or not

---
+++ C#/Check_whether_string_is_empty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_Check_where_string_is_empty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            bool isEmpty = input.Length == 0;
+            Console.WriteLine(isEmpty ? "String is empty" : "String is not empty");
+            Console.Read();
+
+        }
+    }
+}
