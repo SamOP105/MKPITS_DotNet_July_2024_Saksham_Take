3a982d8b2bcaf7364611bff5a4b6f1dadfe17054
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:38:02 +0530

    First Character

---
+++ C#/First_character.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_First_character
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char firstChar = input[0];
+            Console.WriteLine(firstChar);
+            Console.Read();
+
+        }
+    }
+}
