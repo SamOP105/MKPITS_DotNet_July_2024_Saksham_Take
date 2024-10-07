acab50d8ecc98e347598d2003827569cf741be8a
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:21:48 +0530

    Vowels in string

---
+++ C#/Vowels_in_string.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_vowels_in_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hello World";
+            int vowelCount = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = char.ToLower(input[i]);
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                {
+                    vowelCount++;
+                }
+            }
+            Console.WriteLine("Vowel: "+vowelCount);
+            Console.Read();
+        }
+    }
+}
