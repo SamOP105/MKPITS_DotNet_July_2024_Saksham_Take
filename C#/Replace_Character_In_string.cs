4bd130bad8ec0abee5966a06954238cf9c8cb2db
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:27:22 +0530

    Replace_Character_in_string

---
+++ C#/Replace_Character_In_string.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Replace_Characters_in_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hello World";
+            char charToReplace = 'o';
+            char replacementChar = 'a';
+            string result = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == charToReplace)
+                {
+                    result += replacementChar;
+                }
+                else
+                {
+                    result += input[i];
+                }
+            }
+            Console.WriteLine(result); 
+            Console.ReadLine();
+
+        }
+    }
+}
