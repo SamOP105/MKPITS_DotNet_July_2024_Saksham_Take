2973cf14e9615b2533d81f22ba6d0379545830c6
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:24:05 +0530

    Count_whiteSpaces_in_string

---
+++ C#/WhiteSpaces_in_string.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_white_spaces_in_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
+
+            int v = 0, c = 0, d = 0, space = 0;
+
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ')
+                {
+                space++;
+                }
+            }
+            Console.WriteLine("White Spaces: " + space);
+            Console.Read();
+        }
+    }
+}
