7912cd32bb6745739860156001cbc10933534d84
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:51:38 +0530

    Upper case Using Function

---
+++ C#/Uppercase_using_function.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _36_Upper_case_using_function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string s = Console.ReadLine();
+            Console.WriteLine("Trim A string: " + s.ToUpper());
+            Console.ReadLine();
+        }
+    }
+}
