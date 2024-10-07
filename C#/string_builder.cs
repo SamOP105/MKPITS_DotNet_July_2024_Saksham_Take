bbb89e06baeeb605c96796250835b83e4f6667d1
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 16:19:10 +0530

    String Builder Concept

---
+++ C#/string_builder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_builder_concept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb= new StringBuilder("Saksham ");
+            sb.Append("Take");
+            sb.Insert(8,"Prabhakar  ");
+            sb.Remove(18, 1);
+            sb.Replace("Take", "Ingle");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
