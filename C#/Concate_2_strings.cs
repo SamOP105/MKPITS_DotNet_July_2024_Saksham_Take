f0570a82c01cb89f986cd456a7aab9042f012f15
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:30:37 +0530

    Concate 2 strings

---
+++ C#/Concate_2_strings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_concate_2_strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Hello";
+            string str2 = " World";
+            string result = "";
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                result += str1[i];
+            }
+            for (int i = 0; i < str2.Length; i++)
+            {
+                result += str2[i];
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
