ac31ec129d53b53a2dfbc5204f11e0f6df47f7a3
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:42:45 +0530

    First Index of Character

---
+++ C#/First_Index_of_Character.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_Find_the_Index_of_a_Character
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String");
+            string input = Console.ReadLine();
+            char charToFind = 'o';
+            int index = -1;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == charToFind)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            Console.WriteLine(index);
+            Console.ReadLine();
+
+        }
+    }
+}
