80a16b5e1916ea59c606553c0fabc0e7d48cecc0
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:55:20 +0530

    Find Smallest String

---
+++ C#/Find_Smallest_string.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _40_Find_the_Smallest_Word_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string input = Console.ReadLine();
+            string smallestWord = input;
+            string currentWord = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != ' ')
+                {
+                    currentWord += input[i];
+                }
+                else
+                {
+                    if (currentWord.Length > 0 && currentWord.Length < smallestWord.Length)
+                    {
+                        smallestWord = currentWord;
+                    }
+                    currentWord = "";
+                }
+            }
+            if (currentWord.Length > 0 && currentWord.Length < smallestWord.Length)
+            {
+                smallestWord = currentWord;
+            }
+            Console.WriteLine(smallestWord);
+            Console.ReadLine();
+
+        }
+    }
+}
