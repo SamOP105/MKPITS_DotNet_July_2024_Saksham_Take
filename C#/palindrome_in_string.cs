a089dc611c633444f36157d6aa3ece58f7149df4
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:18:20 +0530

    Palindrome in string

---
+++ C#/palindrome_in_string.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string input = Console.ReadLine();
+
+
+            string reversedString = ""; //reverse a string
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                reversedString += input[i];
+            }
+
+
+            if (input == reversedString) //compare
+            {
+                Console.WriteLine("The string is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("The string is not a palindrome.");
+            }
+            Console.Read();
+        }
+    }
+}
