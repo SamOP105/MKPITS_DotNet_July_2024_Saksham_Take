f3c2a31ad034903236d61b3d4663fe58a5e562f1
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:39:48 +0530

    Substring in string

---
+++ C#/Substring in string.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int start = 6; 
+            int length = 5;
+            string substring = "";
+
+            for (int i = 0; i < length; i++)
+            {
+                substring += input[start + i];
+            }
+            Console.WriteLine(substring);
+            Console.Read();
+
+        }
+    }
+}
