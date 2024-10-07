02d1af3133ea8fc8a246291ba5c32702b43551b4
Saksham Take <sakshamtake69@gmail.com>
2024-10-07 21:25:38 +0530

    Remove Spaces from string

---
+++ C#/Remove_spaces_from_string.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_remove_spaces_from_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+            string input = Console.ReadLine();
+            string result = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != ' ')
+                {
+                    result += input[i];
+                }
+            }
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
