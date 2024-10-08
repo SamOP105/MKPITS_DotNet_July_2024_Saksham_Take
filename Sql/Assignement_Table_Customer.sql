4f25dbdb2628cd0d526aa4ef438d18d6ce2e4813
Saksham Take <sakshamtake69@gmail.com>
2024-10-08 20:58:45 +0530

    Assignement Table Orders

---
+++ Sql/Assignement_Table_Customer.sql
@@ -0,0 +1,5 @@
+create table customers (customer_id int primary key identity(0,1), name VARCHAR(100),email VARCHAR(100),country VARCHAR(50));
+insert into customers (name, email, country) values ('Amit Sharma', 'amitsharma@example.com', 'India'),('Priya Patel', 'priyapatel@example.com', 'India'),
+('Rahul Verma', 'rahulverma@example.com', 'India'),('Sneha Nair', 'snehanair@example.com', 'India'),('Rohit Mehta', 'rohitmehta@example.com', 'India'),
+('Pooja Singh', 'poojasingh@example.com', 'India'),('Ankit Gupta', 'ankitgupta@example.com', 'India'),('Neha Kumar', 'nehakumar@example.com', 'India'),
+('Vikram Desai', 'vikramdesai@example.com', 'India'),('Anjali Jain', 'anjalijain@example.com', 'India');
