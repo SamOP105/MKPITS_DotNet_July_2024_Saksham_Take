e29abccc3dec2bcbf29b1dab66cb254ae717fe04
Saksham Take <sakshamtake69@gmail.com>
2024-10-08 23:00:52 +0530

    Find_the_Avg_of_n_numbers

---
+++ C#/Find_the_avg_Of_n_numbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Find_the_average_of_n_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of elements: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter number: ");
+                double num = Convert.ToDouble(Console.ReadLine());
+                sum += num;
+            }
+
+            Console.WriteLine("Average: " + (sum / n));
+            Console.Read();
+        }
+    }
+}
