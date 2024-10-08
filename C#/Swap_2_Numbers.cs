2f11aaa32886c989042d5e978aa73eea4e57217d
Saksham Take <sakshamtake69@gmail.com>
2024-10-08 22:52:33 +0530

    Swap 2 Numbers

---
+++ C#/Swap_2_Numbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Swap_2_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter 2 Numbers: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("a = " + a + ", b = " + b);
+            Console.Read();
+            
+        }
+
+    }
+}
+
