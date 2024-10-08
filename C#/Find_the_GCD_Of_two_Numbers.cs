bbd1814b78fc8e8f788bd40e6812f6bebac0f458
Saksham Take <sakshamtake69@gmail.com>
2024-10-08 22:56:53 +0530

    Find The GCD of 2 Numbers

---
+++ C#/Find_the_GCD_Of_two_Numbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Find_the_GCD_of_two_numbers
+{
+    internal class Program
+    {
+        static int GCD(int a, int b)
+        {
+            if (b == 0) return a;
+            return GCD(b, a % b);
+        }
+        static void Main(string[] args)
+        {
+            int a = 56, b = 98;
+            Console.WriteLine("GCD: " + GCD(a, b));
+            Console.Read();
+            
+        }
+    }
+}
