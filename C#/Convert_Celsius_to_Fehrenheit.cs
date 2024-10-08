248cf5cfa9cfbecfa7f11070c8e787666cc1ea5f
Saksham Take <sakshamtake69@gmail.com>
2024-10-08 22:58:49 +0530

    Convert_Celsius_to_Fehrenheit

---
+++ C#/Convert_Celsius_to_Fehrenheit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Convert_Celsius_to_Fahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius = 25;
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            Console.WriteLine("Fahrenheit: " + fahrenheit);
+            Console.Read();
+        }
+    }
+}
