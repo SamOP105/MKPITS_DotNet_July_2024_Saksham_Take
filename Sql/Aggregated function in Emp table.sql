b369f45b43d4ab53711d2d1ad4ef37001c2e6217
Saksham Take <sakshamtake69@gmail.com>
2024-10-08 11:18:55 +0530

    Aggregated function in Emp Table

---
+++ Sql/Aggregated function in Emp table.sql
@@ -0,0 +1,10 @@
+
+
+
+
+--Aggregate function:
+Select Sum(Salary) from Emp;
+Select Avg(Salary) from Emp;
+Select Max(Salary) from Emp;
+Select Min(Salary) from Emp;
+Select Count(Salary) from Emp;
