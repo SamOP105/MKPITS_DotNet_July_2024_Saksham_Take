e9fd32efdaa43a2315ccb2b300125fa9d8039f6c
Saksham Take <sakshamtake69@gmail.com>
2024-10-08 23:03:12 +0530

    Check_whether_number_is_divisible_by_3_and_5

---
+++ C#/Check if a number is divisible by both 3 and 5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_Check_if_a_number_is_divisible_by_both_3_and_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                Console.WriteLine("Divisible by both 3 and 5");
+            }
+            else
+            {
+                Console.WriteLine("Not divisible by both 3 and 5");
+            }
+            Console.Read();
+        }
+    }
+}
