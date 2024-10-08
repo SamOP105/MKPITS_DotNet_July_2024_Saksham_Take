5c612bc10fb33981f0192b108cd6628ca2b16717
Saksham Take <sakshamtake69@gmail.com>
2024-10-08 22:54:39 +0530

    Check If A Number Is prime

---
+++ C#/Check_If_a_Number_is_prime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Check_if_a_number_is_prime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 17;
+            bool isPrime = true;
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                Console.WriteLine("Prime");
+            }
+            else
+            {
+                Console.WriteLine("Not Prime");
+            }
+        }
+    }
+}
