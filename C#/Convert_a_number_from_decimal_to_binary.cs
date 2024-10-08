746e128db07c9e37500c736291f225df63dca74b
Saksham Take <sakshamtake69@gmail.com>
2024-10-08 23:06:17 +0530

    Convert a number From Decimal to Binary

---
+++ C#/Convert_a_number_from_decimal_to_binary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Convert_a_number_from_decimal_to_binary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a decimal number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            string binary = Convert.ToString(num, 2);
+            Console.WriteLine("Binary: " + binary);
+            Console.Read();
+        }
+    }
+}
