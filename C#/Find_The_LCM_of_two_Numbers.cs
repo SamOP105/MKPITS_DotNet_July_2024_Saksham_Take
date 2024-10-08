14e292af7561ed103aebc792efda8bd28190318f
Saksham Take <sakshamtake69@gmail.com>
2024-10-08 22:57:39 +0530

    Find The LCM of 2 Numbers

---
+++ C#/Find_The_LCM_of_two_Numbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Find_the_LCM_of_two_numbers
+{
+    internal class Program
+    {
+        static int GCD(int a, int b)
+        {
+            if (b == 0) return a;
+            return GCD(b, a % b);
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 15, b = 20;
+            int lcm = (a * b) / GCD(a, b);
+            Console.WriteLine("LCM: " + lcm);
+            Console.Read();
+        
+        }
+    }
+}
