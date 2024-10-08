f9fad1b3132ccd69cde531673fc27a590a0fd42e
Saksham Take <sakshamtake69@gmail.com>
2024-10-08 21:51:27 +0530

    Add 2 Numbers

---
+++ C#/Add_two_numbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Add_two_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter 2 Numbers: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int sum = num1 + num2;
+            Console.WriteLine("Sum: " + sum);
+            Console.Read();
+        }
+    }
+}
