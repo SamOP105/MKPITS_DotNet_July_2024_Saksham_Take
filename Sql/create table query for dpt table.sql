efbac320aa7ec12922cfab9dca0f1e6493ea8e21
Saksham Take <sakshamtake69@gmail.com>
2024-10-08 11:17:02 +0530

    Create table query for dpt table

---
+++ Sql/create table query for dpt table.sql
@@ -0,0 +1,2 @@
+create table Dpt(Did int identity(1,1), Department varchar(20), Email varchar(20));
+INSERT INTO Dpt (Department, Email) VALUES('HR', 'hr@company.com'),('IT', 'it@company.com'),('Sales', 'sales@company.com'),('Marketing', 'marketing@company.com'),('Finance', 'finance@company.com'),('Operations', 'operations@company.com'),('Support', 'support@company.com');
