fd8212a063353916f4da69480eeba7b4347a7186
Saksham Take <sakshamtake69@gmail.com>
2024-10-08 21:03:37 +0530

    Assignment table Department

---
+++ Sql/Assignement_Table_Department.sql
@@ -0,0 +1,3 @@
+create table departments (department_id int primary key identity(0,1),department_name VARCHAR(100));
+insert into departments (department_name) values('Sales'),('HR'),('Engineering'),('Marketing'),('Finance'),('Legal'),('Customer Support'),('Product'),
+('Operations'),('IT');
