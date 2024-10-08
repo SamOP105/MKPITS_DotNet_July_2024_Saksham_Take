f5368c934c3f67bea4dd4a0fd75db269b4aa13d3
Saksham Take <sakshamtake69@gmail.com>
2024-10-08 23:09:51 +0530

    Convert Kilometers to Miles

---
+++ C#/Convert_Kilometers_to_miles.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_Convert_kilometers_to_miles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter kilometers: ");
+            double kilometers = Convert.ToDouble(Console.ReadLine());
+
+            double miles = kilometers * 0.621371;
+            Console.WriteLine("Miles: " + miles);
+            Console.Read();
+        }
+    }
+}
