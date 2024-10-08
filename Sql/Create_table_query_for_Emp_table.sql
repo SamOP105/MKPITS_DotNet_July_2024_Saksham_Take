620d1744702f140d50a6a9b596a585f7aef47aab
Saksham Take <sakshamtake69@gmail.com>
2024-10-08 11:15:06 +0530

    Create table query for Emp table

---
+++ Sql/Create_table_query_for_Emp_table.sql
@@ -0,0 +1,4 @@
+create table Emp(Eid int identity(1,1) , Mobile bigint , Did int);
+
+
+INSERT INTO Emp (Mobile, Did) VALUES (9876543210, 1),(9876543211, 2),(9876543212, 3),(9876543213, 4),(9876543214, 5),(9876543215, 6),(9876543216, 7);
