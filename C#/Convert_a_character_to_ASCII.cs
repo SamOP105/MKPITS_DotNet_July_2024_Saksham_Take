220e3380431806ad5931f4e6ed9df49f85bd5256
Saksham Take <sakshamtake69@gmail.com>
2024-10-08 23:09:03 +0530

    Convert a Character to ASCII value

---
+++ C#/Convert_a_character_to_ASCII.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Convert_a_character_to_its_ASCII_value
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a character: ");
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("ASCII value: " + (int)ch);
+            Console.Read();
+        }
+    }
+}
