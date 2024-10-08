b537680f3c5c4e6bf9bd3cc51fb9fb3d3da0d905
Saksham Take <sakshamtake69@gmail.com>
2024-10-08 21:07:10 +0530

    Assignment table Category

---
+++ Sql/Assignement_Table_Category.sql
@@ -0,0 +1,4 @@
+create table categories (category_id int primary key identity(0,1),category_name VARCHAR(100));
+
+insert into categories (category_name) values('Electronics'),('Accessories'),('Peripherals'),('Furniture'),('Office Supplies'),
+('Software'),('Gaming'),('Home Appliances'),('Outdoor'),('Fitness');
