a389e3f87ec0fc92ba204861c6684cd5b506e6ef
Saksham Take <sakshamtake69@gmail.com>
2024-10-14 17:10:35 +0530

    Format On Orders Table

---
+++ Sql/Format_On_Orders_table.sql
@@ -0,0 +1,3 @@
+--14. Format Date of Birth: Write a query to format the DateOfBirth field as
+--MM/DD/YYYY using the FORMAT function.SELECT ID,FORMAT(ordersDate, 'MM/dd/yyyy') AS FormattedDateOfBirth
+FROM Orders;
