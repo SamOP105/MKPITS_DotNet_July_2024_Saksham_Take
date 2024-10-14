ca0328797575e6fc3bb7a8af44d4cda3e0c1e3f5
Saksham Take <sakshamtake69@gmail.com>
2024-10-14 15:45:57 +0530

    Length Function In String On Customer Table

---
+++ Sql/Len_function_in_string_on_customer_table.sql
@@ -0,0 +1,2 @@
+--9.Check Length of Customer Names: Write a query to select CustomerID,
+--FirstName, and the length of FirstName using the LEN function.select len(firstName) as LFirstName,len(LastName) As LlastName from Customer
