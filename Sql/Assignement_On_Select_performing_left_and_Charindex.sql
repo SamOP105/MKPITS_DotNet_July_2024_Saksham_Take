b66ebe4bdba95f0bbc706566c27bb8e5eea1c13d
Saksham Take <sakshamtake69@gmail.com>
2024-10-14 15:45:05 +0530

    Assignments On Select Performing Left and Charindex

--- Sql/Assignement_On_Select_performing_left_and_Charindex.sql
+++ Sql/Assignement_On_Select_performing_left_and_Charindex.sql
@@ -1,2 +1,2 @@
 --8.Select Email Username Portion: Retrieve the portion of the Email before the '@'
---symbol using the LEFT and CHARINDEX functions.select Left(Email,CHARINDEX('@',Email)-1) from Customer;
+--symbol using the LEFT and CHARINDEX functions.select Left(Email,CHARINDEX('@',Email)-1) from Customer;
