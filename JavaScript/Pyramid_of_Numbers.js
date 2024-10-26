1449ad4b28e61b3bbafd0ff4f2b79e02d33ca4ca
Saksham Take <sakshamtake69@gmail.com>
2024-10-26 23:32:42 +0530

    Pyramid Of Numbers

---
+++ JavaScript/Pyramid_of_Numbers.js
@@ -0,0 +1,18 @@
+function generateNumberPyramid() {
+    const rows = parseInt(document.getElementById("rows").value);
+    if (isNaN(rows) || rows < 1) {
+        document.getElementById("pattern").innerText = "Please enter a valid number of rows.";
+        return;
+    }
+
+    let pattern = "";
+    for (let i = 1; i <= rows; i++) {
+        pattern += " ".repeat(rows - i);
+
+        for (let j = 1; j <= i; j++) {
+            pattern += j + " ";
+        }
+        pattern += "\n";
+    }
+    document.getElementById("pattern").innerText = pattern;
+}
