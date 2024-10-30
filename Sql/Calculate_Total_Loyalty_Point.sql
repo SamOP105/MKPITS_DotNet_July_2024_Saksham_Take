9a5a17939faeb013b966ad8f418c2116743f08bb
Saksham Take <sakshamtake69@gmail.com>
2024-10-30 22:50:12 +0530

    Calculate Total Loyalty Points

---
+++ Sql/Calculate_Total_Loyalty_Point.sql
@@ -0,0 +1,5 @@
+ --Calculate Total Loyalty Points: Write a query to calculate the total sum of
+ --LoyaltyPoints for all customers using the SUM function.
+
+
+select SUM(LoyaltyPoints) as TotalLoyaltyPoints from Customer;
